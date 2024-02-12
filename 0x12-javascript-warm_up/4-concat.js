4e89379bdaab8aa1949f67c32faf66a1e886ea75
youssef2272002salah <youssef22772002salah@gmail.com>
2024-02-12 19:18:15 +0200

    3

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,3 @@
+#!/usr/bin/node
+
+if (process.argv.length <= 2) { console.log('undefined is undefined'); } else if (process.argv.length === 3) { console.log(process.argv[2] + ' is undefined'); } else { console.log(process.argv[2] + ' is ' + process.argv[3]); }
