27dc1f18a8048c655cd589903f266d4bffd33145
youssef2272002salah <youssef22772002salah@gmail.com>
2024-02-12 20:04:23 +0200

    fix

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 function fact (num) {
-  return (num * (num - 1));
+  if (num === 1) { return 1; } else { return (num * fact(num - 1)); }
 }
 
 if (process.argv.length <= 2) { console.log(1); } else { console.log(fact(parseInt(process.argv[2]))); }
--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 
-if (process.argv.length <= 3) { console.log(0); } else {
+if (process.argv.length <= 3) {
+  console.log(0);
+} else {
   const list = process.argv.sort();
-
   console.log(list.reverse()[1]);
 }
