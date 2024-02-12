7b65e8a8a786b25b0cb0c6a4713e4a37bd9f18d0
youssef2272002salah <youssef22772002salah@gmail.com>
2024-02-12 19:19:58 +0200

    fix semi

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,8 +1,5 @@
 #!/usr/bin/node
 
-if (process.argv.length <= 2) 
-{ console.log('Missing number of occurrences'); } 
-else if (!isNaN(process.argv[2])) {
-  for (let i = 0; i < process.argv[2]; i++) 
-  { console.log('C is fun'); }
+if (process.argv.length <= 2) { console.log('Missing number of occurrences'); } else if (!isNaN(process.argv[2])) {
+  for (let i = 0; i < process.argv[2]; i++) { console.log('C is fun'); }
 }
