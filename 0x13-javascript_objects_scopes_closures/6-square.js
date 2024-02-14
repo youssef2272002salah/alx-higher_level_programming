66f3501db536c762d140d62b2565e5d5b72cbeca
youssef2272002salah <youssef22772002salah@gmail.com>
2024-02-14 14:50:22 +0200

    23

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+module.exports = class square extends require('./5-square.js') {
+  charPrint (c) {
+    if (c) {
+      for (let i = 0; i < this.height; i++) {
+        for (let j = 0; j < this.width; j++) { process.stdout.write('C'); }
+        console.log();
+      }
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        for (let j = 0; j < this.width; j++) { process.stdout.write('X'); }
+        console.log();
+      }
+    }
+  }
+};
---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+exports.nbOccurences = function (list, searchElement) {
+  let counter = 0;
+  for (const item of list) {
+    if (item === searchElement) { counter++; }
+  }
+  return counter;
+};
