784c2423a9ed42f529b0d20e4548138f42bd0c37
youssef2272002salah <youssef22772002salah@gmail.com>
2024-02-14 14:52:14 +0200

    fix 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -8,10 +8,7 @@ module.exports = class square extends require('./5-square.js') {
         console.log();
       }
     } else {
-      for (let i = 0; i < this.height; i++) {
-        for (let j = 0; j < this.width; j++) { process.stdout.write('X'); }
-        console.log();
-      }
+      this.print();
     }
   }
 };
