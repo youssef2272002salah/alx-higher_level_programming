6d356d31add03d14a0379578dd8653c42b81ff6f
youssef2272002salah <youssef22772002salah@gmail.com>
2024-02-18 11:46:20 +0200

    10

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,13 +1,19 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 
-import sys 
+"""
+Script that prints the State object with the name passed as argument
+from the database
+Using module SQLAlchemy
+"""
+
+import sys
 from model_state import Base, State
-from sqlalchemy import (create_engine)
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-
-
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+if __name__ == '__main__':
+    engine = \
+        create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1],
+                      sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
     Base.metadata.create_all(engine)
@@ -15,7 +21,7 @@ if __name__ == "__main__":
     state_list = session.query(State).filter(State.name == sys.argv[4]).first()
 
     if state_list:
-        print("{}".format(state_list.id))
+        print('{}'.format(state_list.id))
     else:
-        print("Not found")
+        print('Not found')
     session.close()
--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python3
-
+"""
+Script that lists all State objects that contain the letter a from the database
+Using module SQLAlchemy
+"""
 import sys
 from model_state import Base, State
-from sqlalchemy import (create_engine)
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+if __name__ == '__main__':
+    engine = \
+        create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1],
+                      sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
     Base.metadata.create_all(engine)
 
-    state_list = session.query(State).filter(State.name.like('%a%')).order_by(State.id).all()
+    state_list = session.query(State).filter
+    (State.name.like('%a%')).order_by(State.id).all()
     for i in state_list:
-        print("{}: {}".format(i.id, i.name))
+        print('{}: {}'.format(i.id, i.name))
     session.close()
