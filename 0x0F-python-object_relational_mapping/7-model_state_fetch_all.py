64769a11445e411888812344ca43e70bad4d0a6a
youssef2272002salah <youssef22772002salah@gmail.com>
2024-02-18 11:41:45 +0200

    8

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
-
+"""
+Script that lists all State objects from the database - Using module SQLAlchemy
+"""
 import sys
 from model_state import Base, State
-from sqlalchemy import (create_engine)
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+if __name__ == '__main__':
+    engine = \
+        create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1],
+                      sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
     Base.metadata.create_all(engine)
--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,13 +1,16 @@
-#!/usr/bin/python3
-
+#!/usr/bin/python
+"""
+Script that prints the first State object from the database
+"""
 import sys
 from model_state import Base, State
-from sqlalchemy import (create_engine)
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+if __name__ == '__main__':
+    engine = \
+        create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1],
+                      sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
     Base.metadata.create_all(engine)
@@ -15,7 +18,7 @@ if __name__ == "__main__":
     state_list = session.query(State).order_by(State.id).first()
 
     if state_list:
-        print("{}: {}".format(state_list.id,state_list.name))
+        print('{}: {}'.format(state_list.id, state_list.name))
     else:
-        print("Nothing")
+        print('Nothing')
     session.close()
