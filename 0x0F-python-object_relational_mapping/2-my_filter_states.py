0bfb384634f2055ec3d24315d87c9642cdd041d6
youssef2272002salah <youssef22772002salah@gmail.com>
2024-02-18 11:28:38 +0200

    2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python
-# -*- coding: utf-8 -*-
+"""
+Script that takes in an argument and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument
+"""
 
 import MySQLdb
 from sys import argv
--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,24 +1,20 @@
-#!/usr/bin/python
-
-"""
-Script that takes in an argument and displays all values in the states
-table of hbtn_0e_0_usa where name matches the argument
-"""
+#!/usr/bin/python3
 
 import MySQLdb
 from sys import argv
 
 if __name__ == '__main__':
 
-    db = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3])
+                         
+    cur =db.cursor()
 
-    cur = db.cursor()
-
-    nmeSr = "SELECT * FROM states WHERE name LIKE BINARY '{}'".format(argv[4])
-    cur.execute(nmeSr)
-
-    list = cur.fetchall()
+    
+    query = "SELECT * FROM states WHERE name = %s"
+    cur.execute(query, (argv[4],))
+    
+    list =cur.fetchall()
 
     for i in list:
         print(i)
