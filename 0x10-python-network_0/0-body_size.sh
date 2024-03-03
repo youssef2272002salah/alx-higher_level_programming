09847b993015a33cf4cbad6de41cad3eeacd5c97
youssef2272002salah <youssef22772002salah@gmail.com>
2024-03-03 19:48:41 +0200

    1

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# bash script to know the body size of site
-
-curl -sI $1 | grep "Content-Lenght" | cut -d " " -f2
+# takes in a URL, sends a request to that URL, and displays the size of the body of the response
+curl -sI $1 | grep "Content-Length" | cut -d " " -f2
