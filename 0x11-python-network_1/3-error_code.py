1cf4fc896b643bef5e0072f1518f5c173cf3e0f3
youssef2272002salah <youssef22772002salah@gmail.com>
2024-03-05 23:36:20 +0200

    5

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Sends a request to a given URL and displays the response body."""
+"""Sends a request to a given URL and displays the response body.
+"""
 import sys
 import urllib.error
 import urllib.request
